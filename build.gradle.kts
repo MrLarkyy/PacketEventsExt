2b8046fcd5b6aef0810e8c349350d4da89361176
MrLarkyy_ <studiotadikl@gmail.com>
2024-11-25 01:57:50 +0100

    Remove unused maven repositories and dependencies
    Removed SpigotMC and PaperMC repositories along with the PaperMC dependency. This simplifies the build configuration by keeping only the essential repositories and dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,14 +11,9 @@ repositories {
     mavenCentral()
     maven { url = uri("https://repo.codemc.io/repository/maven-releases/") }
     maven { url = uri("https://repo.codemc.io/repository/maven-snapshots/") }
-    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
-    maven {
-        url = uri("https://repo.papermc.io/repository/maven-public/")
-    }
 }
 dependencies {
     compileOnly("com.github.retrooper:packetevents-spigot:2.6.0")
-    compileOnly("io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
     compileOnly("io.netty:netty-all:4.1.115.Final")
 }
 
